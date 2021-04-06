01ccf8707a0e57449b8fb08f4a9581cb98ce5fa6
Daniel Skowroński <daniel@dsinf.net>
2021-04-06 19:34:22 +0200

    initial commit - old version with all params on CLI
    refer to https://blog.dsinf.net/2020/10/kopie-zapasowe-linuksowej-infrastruktury-i-nie-tylko-duplicati-oraz-jego-monitorowanie-za-pomoca-prometheusa/

---
+++ duplicati_wrapper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+path=$1
+authid=$2
+passphrase=$3
+retention=$4
+
+/bin/date > $path/date.txt # this enforces backup execution
+
+path_encoded="`echo ${path} | base32 | tr '=' '_'`"
+hostname="`hostname`"
+metrics_name="duplicati_${hostname}_`echo $path | tr '/' '_'`"
+metrics_dir="/var/lib/prometheus/node-exporter/"
+
+mkdir -p $metrics_dir
+chown prometheus $metrics_dir
+
+START="$(date +%s)"
+
+EXITCODE=1
+
+for i in `seq 10`
+do
+	/usr/bin/duplicati-cli backup "dropbox:///XXXX/${hostname}${path}?authid=${authid}" "${path}" --backup-name="${path}" --dbpath="/var/duplicati_${path_encoded}.sqlite" --encryption-module=aes --compression-module=zip --dblock-size=50mb --passphrase="${passphrase}" --retention-policy="${retention}" --disable-module=console-password-input 
+	EXITCODE=$?
+
+	if [ $EXITCODE -lt 10 ]
+	then
+		break
+	fi
+
+	sleep $((1 + RANDOM % 60))
+done
+
+
+END="$(date +%s)"
+
+cat << EOF > "$metrics_dir/${metrics_name}.prom.$$"
+${metrics_name}_last_run_start $START
+${metrics_name}_last_run_seconds $(($END - $START))
+${metrics_name}_last_exitcode $EXITCODE
+EOF
+
+mv "$metrics_dir/${metrics_name}.prom.$$" "$metrics_dir/${metrics_name}.prom"
