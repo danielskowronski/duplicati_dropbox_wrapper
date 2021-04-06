89aab17d1e5bda2498da6e94e1f777247f13f133
Daniel Skowroński <daniel@dsinf.net>
2021-04-06 20:16:50 +0200

    source config from /etc, add repair command support

--- README.md
+++ README.md
@@ -13,3 +13,46 @@ Example file `duplicati_wrapper.conf` should be placed in `/etc/`. It's simply *
 * `DUPLICATI_RETENTION` - see https://duplicati.readthedocs.io/en/stable/06-advanced-options/#retention-policy (the default one `1W:1D,4W:1W,12M:1M` is *smart* in Duplicati GUI)
 * `DROPBOX_MASTERPATH` - folder inside `/Applications/Duplicati/` to place backup folder, that is named after hostname of machine on which script executes
 * `DROPBOX_AUTHID` - see https://duplicati-oauth-handler.appspot.com/
+
+
+## Usage examples
+
+### Usage
+```
+$ /opt/duplicati_wrapper.sh --help
+usage: /opt/duplicati_wrapper2.sh <backup,repair> SRC_PATH
+$
+```
+
+### Backup
+```
+$ /opt/duplicati_wrapper.sh backup /var/lib/postgresql/
+Backup started at 04/06/2021 18:12:23
+Checking remote backup ...
+  Listing remote folder ...
+Scanning local files ...
+  1953 files need to be examined (78.80 MB)
+  Uploading file (1021 bytes) ...
+  Uploading file (78.81 KB) ...
+  Uploading file (925 bytes) ...
+  0 files need to be examined (0 bytes)
+Checking remote backup ...
+  Listing remote folder ...
+Verifying remote backup ...
+Remote backup verification completed
+  Downloading file (78.81 KB) ...
+  Downloading file (925 bytes) ...
+  Downloading file (1021 bytes) ...
+  Duration of backup: 00:00:11
+  Remote files: 38
+  Remote size: 8.58 MB
+  Total remote quota: 0 bytes
+  Available remote quota: 0 bytes
+  Files added: 0
+  Files deleted: 0
+  Files changed: 1
+  Data uploaded: 80.71 KB
+  Data downloaded: 80.71 KB
+Backup completed successfully!
+$
+```
--- duplicati_wrapper.sh
+++ duplicati_wrapper.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 
-path=$1
-authid=$2
-passphrase=$3
-retention=$4
+ACTION=$1
+SRC_PATH=$2
 
-/bin/date > $path/date.txt # this enforces backup execution
+source /etc/duplicati_wrapper.conf
 
-path_encoded="`echo ${path} | base32 | tr '=' '_'`"
-hostname="`hostname`"
-metrics_name="duplicati_${hostname}_`echo $path | tr '/' '_'`"
-metrics_dir="/var/lib/prometheus/node-exporter/"
-
-mkdir -p $metrics_dir
-chown prometheus $metrics_dir
-
-START="$(date +%s)"
-
-EXITCODE=1
-
-for i in `seq 10`
-do
-	/usr/bin/duplicati-cli backup "dropbox:///amaterasu/${hostname}${path}?authid=${authid}" "${path}" --backup-name="${path}" --dbpath="/var/duplicati_${path_encoded}.sqlite" --encryption-module=aes --compression-module=zip --dblock-size=50mb --passphrase="${passphrase}" --retention-policy="${retention}" --disable-module=console-password-input 
-	EXITCODE=$?
-
-	if [ $EXITCODE -lt 10 ]
-	then
-		break
-	fi
-
-	sleep $((1 + RANDOM % 60))
-done
+PATH_ENCODED="`echo ${SRC_PATH} | base32 | tr '=' '_'`"
+HOSTNAME="`hostname`"
 
 
-END="$(date +%s)"
+if [[ "$1" ==  "backup" ]]; then
+    metrics_name="duplicati_${HOSTNAME}_`echo $SRC_PATH | tr '/' '_'`"
+    metrics_dir="/var/lib/prometheus/node-exporter/"
 
-cat << EOF > "$metrics_dir/${metrics_name}.prom.$$"
+    mkdir -p $metrics_dir
+    chown prometheus $metrics_dir
+
+    /bin/date > $SRC_PATH/date.txt # this enforces backup execution
+
+    START="$(date +%s)"
+
+    EXITCODE=1
+
+    for i in `seq 10`
+    do
+    	/usr/bin/duplicati-cli backup "dropbox:///${DROPBOX_MASTERPATH}/${HOSTNAME}${SRC_PATH}?authid=${DROPBOX_AUTHID}" "${SRC_PATH}" --backup-name="${SRC_PATH}" --dbpath="/var/duplicati_${PATH_ENCODED}.sqlite" --encryption-module=aes --compression-module=zip --dblock-size=50mb --passphrase="${DUPLICATI_PASSPHRASE}" --retention-policy="${DUPLICATI_RETENTION}" --disable-module=console-password-input 
+    	EXITCODE=$?
+
+    	if [ $EXITCODE -lt 10 ]
+    	then
+    		break
+    	fi
+
+    	sleep $((1 + RANDOM % 60))
+    done
+
+    END="$(date +%s)"
+
+    cat << EOF > "$metrics_dir/${metrics_name}.prom.$$"
 ${metrics_name}_last_run_start $START
 ${metrics_name}_last_run_seconds $(($END - $START))
 ${metrics_name}_last_exitcode $EXITCODE
 EOF
 
-mv "$metrics_dir/${metrics_name}.prom.$$" "$metrics_dir/${metrics_name}.prom"
+    mv "$metrics_dir/${metrics_name}.prom.$$" "$metrics_dir/${metrics_name}.prom"
+
+elif [[ "$1" ==  "repair" ]]; then
+    /usr/bin/duplicati-cli repair "dropbox:///${DROPBOX_MASTERPATH}/${HOSTNAME}${SRC_PATH}?authid=${DROPBOX_AUTHID}" --backup-name="${SRC_PATH}" --dbpath="/var/duplicati_${PATH_ENCODED}.sqlite" --encryption-module=aes --compression-module=zip --dblock-size=50mb --passphrase="${DUPLICATI_PASSPHRASE}" --retention-policy="${DUPLICATI_RETENTION}" --disable-module=console-password-input 
+else
+    echo "usage: $0 <backup,repair> SRC_PATH"
+fi
